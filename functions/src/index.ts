236f894f27eb56f6a8ff74f703743b0525929fba
CristianX <thecristianx@hotmail.com>
2020-04-10 18:55:52 -0500

    Obteniendo data de la bd en index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,12 +14,37 @@ admin.initializeApp({
 
 })
 
+
+// Referencia a la base de datos
+const db = admin.firestore();
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+// Cloud function
 export const helloWorld = functions.https.onRequest((request, response) => {
     // va con json para que regrese un archivo json
     response.json({
         mensaje: 'Hola mundo desde funciones de Firebase!!'
     });
+ });
+
+ // Cloud function; asyng por que el docsSnap es asíncrono
+export const getGOTY = functions.https.onRequest( async (request, response) => {
+    // const nombre = request.query.nombre || 'Sin nombre';
+    
+    // Referencia al nobo principal de la db
+    const gotyRef = db.collection('goty');
+
+    // Snapshot (no tiempo real)  de la bd; necesita el await por que es asíncrono
+    const docsSnap = await gotyRef.get();
+
+    // Llamando data
+    const juegos = docsSnap.docs.map( doc => doc.data() );
+
+    // Obteniendo datos de la posición 0 de la data de la db
+    //response.json( docsSnap.docs[0].data() );
+
+    // Llamando juegos
+    response.json( juegos );
+
  });
