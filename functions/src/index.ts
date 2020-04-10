0c7c4eafa88cdd86a27d170cbdb6b53b00ad166a
CristianX <thecristianx@hotmail.com>
2020-04-10 18:37:44 -0500

    Agregando credenciales para trabajar local y remoetamente; inicializando la app con valores de la cuenta de servicio y descargando el serviceAccountKey

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,19 @@
 import * as functions from 'firebase-functions';
 
+// Firebase Admin para llamar la colección de la database
+import * as admin from 'firebase-admin';
+
+// Para el serviceAccount generar una nueva clave privada, el archivo descargado pegarlo en lib y en src
+const serviceAccount = require("./serviceAccountKey.json");
+
+
+// Inicialización de la aplicación; para ir a estas credenciales ir al engrane de información general, configuracion de proyecto y cuentas de servicio, elegir node.js
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://firestore-grafica-6feb1.firebaseio.com"
+
+})
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
